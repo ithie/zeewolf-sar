88616e045de1e757cc5850d7e68c438b93fb3a40
Michael Draws-Beer <michael.draws-beer@cybercon.de>
2026-03-10 05:58:37 +0100

    Added collisionboxes.

---
+++ src/game/render.ts
@@ -0,0 +1,18 @@
+const iso = (
+    vx: number,
+    vy: number,
+    h: number,
+    cx: number,
+    cy: number,
+    { canvas, tileW, tileH, stepH }: { canvas: HTMLCanvasElement; tileH: number; tileW: number; stepH: number }
+) => {
+    let cv = canvas || document.getElementById('gameCanvas');
+    return {
+        x: cv.width / 2 + (vx - vy) * (tileW / 2) - cx,
+        y: cv.height / 2 + (vx + vy) * (tileH / 2) - h * stepH - cy,
+    };
+};
+
+export { iso };
+
+window.iso = iso;
